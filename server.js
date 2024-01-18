db10a90b7ea0ecf398203e16751bc5371e232c3d
shaimeur <haimeursaad@gmail.com>
2024-01-18 01:06:33 +0100

    implementing a basic routing and evaluating the url with the URL buildIn module sending header information with writeHead method

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const url = require("url")
 const port = 8000;
 // console.log(http.METHODS.length)
 
@@ -6,7 +7,20 @@ const port = 8000;
 //     console.log(m)
 // }
 const server = http.createServer((req,res)=>{
-    res.end('hello from server!')
+    const pathName = req.url
+    if(pathName === "/" || pathName === '/overview'){
+
+        res.end('hello from Overview!')
+    }else if (pathName === '/product'){
+
+        res.end('hello from product!')
+    }else{
+        res.writeHead(404,{
+            "content-type" : "text/html",
+            "my-own-header" : "hello-world"
+        })
+        res.end("<h1>Page not found</h1>")
+    }
 })
 
 server.listen( port,"127.0.0.1",()=>{

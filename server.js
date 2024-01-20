891a90fc3ba39aee0afbc8011bc32a66a3889060
shaimeur <haimeursaad@gmail.com>
2024-01-20 01:55:56 +0100

    making the templates folder content dynamic

--- server.js
+++ server.js
@@ -7,19 +7,57 @@ const port = 8000;
 // for(let m of http.METHODS){
 //     console.log(m)
 // }
-const data = fs.readFileSync(`${__dirname}/starters/fs-starter/dev-data/data.json`)
 
- JSON.parse(data)
+
+const replaceTemplate = (template,product)=>{
+        let output = template.replace(/{%PRODUCTNAME%}/g,product.productName)
+
+        output = output.replace(/{%IMAGE%}/g,product.image)
+        output = output.replace(/{%PRICE%}/g,product.price)
+        output = output.replace(/{%FROM%}/g,product.from)
+        output = output.replace(/{%NUTRIENTS%}/g,product.nutrients)
+        output = output.replace(/{%QUANTITY%}/g,product.quantity)
+        output = output.replace(/{%DESCRIPTION%}/g,product.description)
+        output = output.replace(/{%ID%}/g,product.id)
+
+        if(!product.organic){
+            output = output.replace(/{%NOT_ORGANIC%}/g,'not-organic')
+        }
+
+        return output
+    }
+
+const overview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8')
+const product = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8')
+const card = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8')
+
+const data = fs.readFileSync(`${__dirname}/fs/dev-data/data.json`,'utf-8')
+ const dataObj = JSON.parse(data)
 
 const server = http.createServer((req,res)=>{
     const pathName = req.url
+
+    // OVERVIEW PAGE :
+
     if(pathName === "/" || pathName === '/overview'){
+        res.writeHead(200,{
+            "Content-type" : "text/html"
+        })
 
-        res.end('hello from Overview!')
-    }else if (pathName === '/product'){
-
-        res.end('hello from product!')
-    }else if (pathName === "/api"){
+       const cardHtml =   dataObj.map((item)=>replaceTemplate(card,item)).join('')
+        // console.log(cardHtml)
+        const output = overview.replace('{%PRODUCT_CARDS%}',cardHtml)
+        res.end(output)
+    }
+        // Product Page
+    else if (pathName === '/product'){
+        res.writeHead(200,{
+            "Content-type" : "text/html"
+        })
+        res.end(product)
+    }
+        // API
+    else if (pathName === "/api"){
         //  fs.readFile("./starters/fs-starter/dev-data/data.json",'utf-8',(err,data)=>{
         //     if(err){
         //         res.end("errror!!")
@@ -30,10 +68,11 @@ const server = http.createServer((req,res)=>{
                 })
              res.end(data)
 
-    }else{
+    }
+        // Not Found
+    else{
         res.writeHead(404,{
-            "Content-type" : "text/html",
-            "my-own-header" : "hello-world"
+            "Content-type" : "text/html"
         })
         res.end("<h1>Page not found</h1>")
     }
